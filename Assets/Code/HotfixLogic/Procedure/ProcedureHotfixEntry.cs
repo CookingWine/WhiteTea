54e05e5cec14d216d65e2d3f73bee4c0f006def2
cookingwine <FindWindPeople@163.com>
2023-10-23 22:35:16 +0800

    1、添加游戏流程

---
+++ Assets/Code/HotfixLogic/Procedure/ProcedureHotfixEntry.cs
@@ -0,0 +1,17 @@
+using UnityGameFramework.Runtime;
+using ProcedureOwner = UGHGame.HotfixLogic.IFsm;
+namespace UGHGame.HotfixLogic
+{
+    /// <summary>
+    /// 热更流程的入口
+    /// </summary>
+    public class ProcedureHotfixEntry:ProcedureBase
+    {
+        protected internal override void OnEnter(ProcedureOwner procedureOwner)
+        {
+            base.OnEnter(procedureOwner);
+            Log.Info("进入热更流程的入口了");
+        }
+
+    }
+}
