0812b8a928be15b4233d3b8f824bf5959ad55be7
阿宅想要养只猫 <FindWindPeople@163.com>
2023-12-29 16:07:25 +0800

    add code

---
+++ Assets/Code/HotfixLogic/EventArgs/HotfixGameEventArgs.cs
@@ -0,0 +1,17 @@
+namespace UGHGame.HotfixLogic
+{
+    /// <summary>
+    /// 事件参数
+    /// </summary>
+    public abstract class HotfixGameEventArgs:IReference
+    {
+        /// <summary>
+        /// 获取类型编号。
+        /// </summary>
+        public abstract int Id
+        {
+            get;
+        }
+        public abstract void Clear( );
+    }
+}
