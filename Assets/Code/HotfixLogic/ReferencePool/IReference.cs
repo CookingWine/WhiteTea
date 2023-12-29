659d1d74488f64a49a168d0e98dcf8d1d62935c6
阿宅想要养只猫 <FindWindPeople@163.com>
2023-12-29 15:17:25 +0800

    添加引用池

---
+++ Assets/Code/HotfixLogic/ReferencePool/IReference.cs
@@ -0,0 +1,13 @@
+namespace UGHGame.HotfixLogic
+{
+    /// <summary>
+    /// 引用池接口
+    /// </summary>
+    public interface IReference
+    {
+        /// <summary>
+        /// 清理引用
+        /// </summary>
+        void Clear( );
+    }
+}
