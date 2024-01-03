9d00183113fc951ddd38499107a2ce47a61763a3
阿宅想要养只猫 <FindWindPeople@163.com>
2024-01-03 15:56:49 +0800

    1、添加配置

--- Assets/Code/Editor/Utility/GameEditorUtility.cs
+++ Assets/Code/Editor/Utility/GameEditorUtility.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.IO;
+using UGHGame.BuiltinRuntime;
 using UnityEngine;
 
 namespace UGHGame.GameEditor
@@ -25,6 +26,10 @@ namespace UGHGame.GameEditor
             IsEnable = isEnable;
         }
     }
+
+    /// <summary>
+    /// Editor路径工具
+    /// </summary>
     internal class EidtorPathUtility
     {
         public static string[] GetAllFile(string path)
@@ -49,5 +54,61 @@ namespace UGHGame.GameEditor
             }
             return list.ToArray( );
         }
+    }
+
+    /// <summary>
+    /// 编辑器内的配置项
+    /// </summary>
+    internal class ConstEditor
+    {
+        /// <summary>
+        /// 新建脚本时自动修改脚本编码方式为utf-8以支持中文
+        /// </summary>
+        public const bool AutoScriptUTF8 = true;
+
+        /// <summary>
+        /// 内置运行的程序集
+        /// </summary>
+        public const string BuiltinRuntimeAssembly = "Assets/Code/BuiltinRuntime/UGHGame.BuiltinRuntime.asmdef";
+        /// <summary>
+        /// 热更程序集
+        /// </summary>
+        public const string HotfixLogicAssembly = "Assets/Code/HotfixLogic/UGHGame.HotfixLogic.asmdef";
+
+        /// <summary>
+        /// 数据表的目录
+        /// </summary>
+        public static string DataTableExcelPath
+        {
+            get
+            {
+                return AssetUtility.GetCombinePath(Directory.GetParent(Application.dataPath).FullName , "HotfixAssets/DataTables");
+            }
+        }
+        /// <summary>
+        /// 本地化语言表
+        /// </summary>
+        public static string LanguageExcelPath
+        {
+            get
+            {
+                return AssetUtility.GetCombinePath(Directory.GetParent(Application.dataPath).FullName , "HotfixAssets/Localization");
+            }
+        }
+
+        /// <summary>
+        /// 数据表的存放位置
+        /// </summary>
+        public const string DataTablePath = "Assets/HotfixAssets/DataTables";
+
+        /// <summary>
+        /// 语言配置表的存放位置
+        /// </summary>
+        public const string LanguageTablePath = "Assets/HotfixAssets/Localization";
+
+        /// <summary>
+        /// 数据表的生成代码位置
+        /// </summary>
+        public const string DataTableCodePath = "Assets/Code/HotfixLogic/DataTable/CodeGenerate";
     }
 }
