e3254d40b71a0228d45ce8186678ffaf62467044
阿宅想要养只猫 <FindWindPeople@163.com>
2024-01-03 16:23:33 +0800

    1、删除资源

--- Assets/Code/Editor/GameFrameworkExtend/DataTableGenerator/DataTableCodeGenerator.cs
+++
@@ -1,5 +0,0 @@
-using System.Text;
-namespace UGHGame.GameEditor
-{
-    public delegate void DataTableCodeGenerator(DataTableProcessor dataTableProcessor , StringBuilder codeContent , object userData , bool isHotfixCSharp);
-}
